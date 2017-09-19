986d019fdd96076b53265bb8a6b47141c2a4a2b5
Samantha Ngo <sngo1@stuy.edu>
2017-09-19 12:20:42 -0400

    editing job_class_percentage

--- Tests & Alternate Codes/job_class_percentage.py
+++ Tests & Alternate Codes/job_class_percentage.py
@@ -11,21 +11,30 @@ OCCUPATIONS = {};
 def csvToDict(file) :
 	for line in open(file, 'r'):
 		currentLine = line;
-		if currentLine.find('"') :
+		print currentLine
+		if -1 != currentLine.find('"') :
+			comma = currentLine.find(",");
 			startQuote = currentLine.find('"');
-			endQuote = currentLine[1].find('"');
+			print startQuote
+			endQuote = currentLine[1:].find('"');
+			print endQuote
 			# Takes out the occupation from the quotes.
-			occupation = currentLine[1:endQuote];
-			percentage = currentLine[endQuote + 1:];
+			occupation = currentLine[1:comma - 1];
+			print occupation
+			percentage = currentLine[comma + 1:len(currentLine) - 1];
+			#print "Percentage: " + percentage;
+			#percentage = float(percentage);
 		else:
 			# Determines the occupation and the percentage
-			quote = currentLine.find('"');
-			occupation = currentLine[:quote];
-			percentage = currentLine[quote + 1:];
+			comma = currentLine.find(',');
+			occupation = currentLine[:comma];
+			percentage = currentLine[comma + 1:len(currentLine) - 1];
+			#print "Percentage: " + percentage;
+			#percentage = float(percentage);
 		# Adds occupation and percentage to the dictionary accordingly.
 		OCCUPATIONS[occupation] = percentage;
 
-csvToDict('occupation.csv');
+csvToDict('occupations.csv');
 print OCCUPATIONS;
 
 # Ways to approach:
@@ -45,17 +54,16 @@ occupationAttached = [];
 # for user to change dictionary if necessary, but with same format.
 def randomOccupation():
 	# Counter for iterating through lists simultaneously.
-	a = 0;
+	# a = 0;
 	# Acts as a start and end for each occupation's range.
 	sum = 0;
 	# For each key, determine the unique range from 1-1000 for that occupation
 	# based on its percentage and add it to the lists.
 	for k in OCCUPATIONS:
-		if !(k == "Job Class" || k == "Total"):
-			occupationAttached[a] = k;
+		if ~(k == "Job Class" or k == "Total"):
+			occupationAttached.append(k);
 			sum += OCCUPATIONS[k] * 10;
-			occupationRange[a] = sum;
-			a += 1;
+			occupationRange.append(sum);
 	# Pick a random number from 1-998
 	num = random.randint(1,998);
 	# Find the range it falls into and the occupation associated with that 
@@ -64,4 +72,10 @@ def randomOccupation():
 	while num > occupationRange[b]:
 		b += 1;
 	return occupationAttached[b];
+
+print randomOccupation();
+print randomOccupation();
+print randomOccupation();
+print randomOccupation();
+print randomOccupation();
 		
